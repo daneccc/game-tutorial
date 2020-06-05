371c2341351b8714a4b299e7c75965db6f9a98e1
daneccf <danielecavalcante@alu.ufc.br>
2020-06-05 20:53:48 -0300

    game loop done

--- TutorialGame/GameLoop/GameLoop.cs
+++ TutorialGame/GameLoop/GameLoop.cs
@@ -3,10 +3,82 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SFML.Graphics;
+using SFML.Window;
+using SFML.System;
 
-namespace TutorialGame.GameLoop
+namespace TutorialGame
 {
-    class GameLoop
+    public abstract class GameLoop
     {
+
+        public const int TARGET_FPS = 60;
+        public const float TIME_UNTIL_UPDATE = 1f / TARGET_FPS;
+
+        public RenderWindow Window
+        {
+            get;
+            protected set;
+        }
+
+        public GameTime GameTime
+        {
+            get;
+            protected set;
+        }
+
+        //color that will display in a window when there is nothing
+        public Color WindowClearColor
+        {
+            get;
+            protected set;
+        }
+
+        public GameLoop(uint windowWidth, uint windowHeight, string windowTitle, Color windowClearColor)
+        {
+            this.WindowClearColor = windowClearColor;
+            this.Window = new RenderWindow(new VideoMode(windowWidth, windowHeight), windowTitle);
+            this.GameTime = new GameTime();
+        }
+
+        public void Run() {
+
+            LoadContent();
+            Initialize();
+
+            float totalTimeBeforeUpdate = 0f;
+            float previousTimeElpsed = 0f;
+            float deltaTime = 0f;
+            float totalTimeElapsed = 0f;
+
+            Clock clock = new Clock(); //starts automatically
+
+            while (Window.IsOpen) {
+                Window.DispatchEvents();
+
+                totalTimeElapsed = clock.ElapsedTime.AsSeconds();
+                deltaTime = totalTimeElapsed - previousTimeElpsed;
+                previousTimeElpsed = totalTimeElapsed;
+
+                totalTimeBeforeUpdate += deltaTime;
+
+                if(totalTimeBeforeUpdate >= TIME_UNTIL_UPDATE) {
+                    GameTime.Update(totalTimeBeforeUpdate, clock.ElapsedTime.AsSeconds());
+                    totalTimeBeforeUpdate = 0f;
+
+                    Update(GameTime);
+
+                    Window.Clear(WindowClearColor);
+                    Draw(GameTime);
+                    Window.Display();
+
+                }
+            }
+        }
+
+        public abstract void LoadContent();
+        public abstract void Initialize();
+        public abstract void Update(GameTime gameTime);
+        public abstract void Draw(GameTime gameTime);
     }
 }
--- TutorialGame/GameLoop/GameTime.cs
+++ TutorialGame/GameLoop/GameTime.cs
@@ -4,9 +4,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TutorialGame.GameLoop
+namespace TutorialGame
 {
-    class GameTime
+    public class GameTime
     {
+
+        private float _deltaTime = 0f;
+        private float _timeScale = 1f;
+
+        public float TimeScale
+        {
+            get { return _timeScale; }
+            set { _timeScale = value; }
+        }
+
+        public float DeltaTime
+        {
+            get { return _deltaTime * _timeScale; }
+            set { _deltaTime = value; }
+        }
+
+        public float DeltaTimeUnscaled
+        {
+            get { return _deltaTime; }
+        }
+
+        public float TotalTimeElapsed
+        {
+            get;
+            private set;
+        }
+
+        public GameTime()
+        {
+        }
+
+        public void Update(float deltaTime, float totalTimeElapsed)
+        {
+            _deltaTime = deltaTime;
+            TotalTimeElapsed = totalTimeElapsed;
+        }
     }
 }
