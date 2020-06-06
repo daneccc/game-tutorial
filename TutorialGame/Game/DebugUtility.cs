4fe276e650c52e4cdab3d50d101badaff39f1da5
daneccf <danielecavalcante@alu.ufc.br>
2020-06-05 22:35:21 -0300

    finished

---
+++ TutorialGame/Game/DebugUtility.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Graphics;
+using SFML.System;
+
+namespace TutorialGame.Game {
+    public static class DebugUtility {
+
+        public const string CONSOLE_FONT_PATH = "./fonts/arial.ttf";
+
+        public static Font consoleFont;
+
+        public static void LoadContent() {
+
+            consoleFont = new Font(CONSOLE_FONT_PATH);
+        }
+
+        public static void DrawPerfomanceData(GameLoop gameLoop, Color fontColor) {
+
+            if (consoleFont == null)
+                return;
+
+            string totalTimaElapsedStr = gameLoop.GameTime.TotalTimeElapsed.ToString("0.000");
+            string deltaTimeStr = gameLoop.GameTime.DeltaTime.ToString("0.00000");
+            float fps = 1f/gameLoop.GameTime.DeltaTime;
+            string fpsStr = fps.ToString("0.00");
+
+            Text textA = new Text(totalTimaElapsedStr, consoleFont, 14);
+            textA.Position = new Vector2f(4f, 8f);
+            textA.Color = fontColor;
+            
+            Text textB = new Text(deltaTimeStr, consoleFont, 14);
+            textB.Position = new Vector2f(4f, 28f);
+            textB.Color = fontColor;
+            
+            Text textC = new Text(fpsStr, consoleFont, 14);
+            textC.Position = new Vector2f(4f, 48f);
+            textC.Color = fontColor;
+
+            gameLoop.Window.Draw(textA);
+            gameLoop.Window.Draw(textB);
+            gameLoop.Window.Draw(textC);
+
+        }
+    }
+}
--- TutorialGame/Game/TutorialGame.cs
+++ TutorialGame/Game/TutorialGame.cs
@@ -7,7 +7,7 @@ using SFML.Audio;
 using SFML.Graphics;
 using SFML.System;
 using SFML.Window;
-    
+using TutorialGame.Game;
 
 namespace TutorialGame {
     public class TutorialGame : GameLoop {
@@ -15,17 +15,19 @@ namespace TutorialGame {
         public const uint DEFAULT_WINDOW_WIDTH = 640;
         public const uint DEFAULT_WINDOW_HEIGHT = 480;
         public const string WINDOW_TITLE = "Tutorial Game";
-        public TutorialGame() : base (DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT, WINDOW_TITLE, Color.Red) {
+        public TutorialGame() : base (DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT, WINDOW_TITLE, Color.Black) {
 
         }
 
         public override void Draw(GameTime gameTime) {
+            DebugUtility.DrawPerfomanceData(this, Color.White);
         }
 
         public override void Initialize() {
         }
 
         public override void LoadContent() {
+            DebugUtility.LoadContent();
         }
 
         public override void Update(GameTime gameTime) {
